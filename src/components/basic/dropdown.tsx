f12b83fd7865f07d20e51744e2e4b2b73449a379
mo <munkh-orgil@hackum.com>
2024-05-12 04:38:33 +0800

    feat:added rightNavbar

--- src/components/basic/dropdown.tsx
+++ src/components/basic/dropdown.tsx
@@ -1,25 +1,52 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import Select from "react-tailwindcss-select";
+import {XCircle} from "lucide-react";
 
 type DropDownBoxProps = {
-  data : string | undefined
+  data : any
+  handleClose:(data : any) => void
 }
-const DropDownBox = ({ data } : DropDownBoxProps ) => {
-  const [animal, setAnimal] = useState(null);
-  console.log(data)
-  const handleChange = (value: any) => {
-    console.log("value:", value);
-    setAnimal(value);
-  };
+const DropDownBox = ({ data , handleClose } : DropDownBoxProps ) => {
+  const [option , setOption] = useState<any>()
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://192.168.0.112:8080/api/v1/grades/filter-options/${data.value}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        let jsonData = await response.json();
+        jsonData = jsonData.map((data: any) => ({value: `${data.value}`, label: `${data.label}`}))
+        setOption(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const [select, setSelect] = useState<any>(null);
+  const handleChange = (value: any) => {
+    setSelect(value);
+    console.log('value' , value)
+  };
   return (
-    <Select
-      value={animal}
-      onChange={handleChange}
-      options={data}
-      primaryColor={""}
-      placeholder='Бүгд'
-    />
+      <>
+        <div className='text-sm flex justify-between items-center'>{data.label} <XCircle className='cursor-pointer'
+                                                                                         onClick={() => handleClose(data)}/>
+        </div>
+        <Select
+            value={ select && select.map((selects:any) => ({ value : selects.value , label : `${selects.value.length >= 10 ? selects.label.substring(0 , 10 ) + '...' : selects.value}`} )) }
+            onChange={handleChange}
+            options={option}
+            primaryColor={""}
+            placeholder='Бүгд'
+            isSearchable={true}
+            isMultiple={true}
+        />
+      </>
+
   );
 };
 
--- src/components/sideBarRight.tsx
+++ src/components/sideBarRight.tsx
@@ -2,30 +2,44 @@
 
 import { useEffect, useState} from "react";
 import DropDownBox from "./basic/dropdown";
+import {PlusSquare} from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent, DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@radix-ui/react-dropdown-menu";
 
 // function classNames(...classes: string[]) {
 //   return classes.filter(Boolean).join(" ");
 // }
 
 export default function SidebarRight() {
-    const [data, setData] = useState();
-
+    const [data, setData] = useState<any>();
+    const [filter , setFilter] = useState<any>([]);
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://192.168.0.112:8080/api/v1/grades/options');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const jsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
         fetchData();
     }, []);
-    const fetchData = async () => {
-        try {
-            const response = await fetch('http://192.168.0.112:8080/api/v1/staffs/options');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const jsonData = await response.json();
-            setData(jsonData);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-    console.log(data)
+     function addFilter (datas : any)  {
+        setFilter((st: any) => [...st, datas]);
+        setData((el: any) => el.filter((datum: any) => datum.value != datas.value))
+    }
+    function handleClose(data:any) {
+        setFilter((el: any) => el.filter((datum: any) => datum.value != data.value))
+        setData((st: any) => [...st, data])
+    }
   return (
     <div className="flex grow flex-col gap-y-5 h-full overflow-y-auto bg-[#154185] px-6 text-indigo-200">
       <div className="flex h-16 shrink-0 items-center justify-end">
@@ -35,15 +49,19 @@ export default function SidebarRight() {
           alt="Your Company"
         />
       </div>
-      <nav className="flex flex-1 flex-col">
+      <nav className="flex  flex-1 flex-col">
           <form className='space-y-4'>
-              <ul >
-                  <div className='pb-2'>Тэнхим сонгох</div>
-                  <DropDownBox  data={data.id} />
-              </ul>
-              <ul>
-                  <div className='pb-2'>Хөтөлбөр сонгох</div>
-                  <DropDownBox/>
+              {filter?.map((filters:any) => <DropDownBox key={filters.value} handleClose={(data:any) => handleClose(data)} data={filters} />)}
+              <ul className='flex flex-col space-y-2 items-center justify-center'>
+                  <div className='text-lg font-medium'>Filter add</div>
+                  <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                          <button><PlusSquare/></button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className='bg-white cursor-pointer w-full text-[#154185] rounded-md focus:outline-none p-1' >
+                          {data?.map((datas:any) => <DropdownMenuItem onClick={() => addFilter(datas)} className='p-2 hover:bg-[#154185]/20 rounded-md' key={datas.value}>{datas.label}</DropdownMenuItem>)}
+                      </DropdownMenuContent>
+                  </DropdownMenu>
               </ul>
           </form>
       </nav>
